4567883b040b35193e0ae7ab5e11abf46519510e
SuberSeb <suberseb@gmail.com>
2019-11-01 18:26:55 +0300

    remove test commit

--- src/main/java/ru/user/lab04/servletapp/servlet/IndexServlet.java
+++ src/main/java/ru/user/lab04/servletapp/servlet/IndexServlet.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//test commit
-
-
 @WebServlet("")
 public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
